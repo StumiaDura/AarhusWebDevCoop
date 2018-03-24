eb821c8fd17224c8bd6c83204ec328a3d47b5299
luko\Lukas <lukas.klinga1994@gmail.com>
2018-03-24 09:52:16 +0100

    First commit with the project files

---
+++ source/AarhusWebDevCoop2/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Aarhus_Web.ViewModels;
+using System.Net.Mail;
+
+namespace Aarhus_Web.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("Contact Form", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            MailMessage message = new MailMessage();
+            message.To.Add("lukas.klinga1994@gmail.com");
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message;
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("dobos.radu@gmail.com", "yxhmlgbpnwhgrtdl");
+                smtp.EnableSsl = true;
+                // send mail
+                smtp.Send(message);
+            }
+
+            return RedirectToCurrentUmbracoPage();
+        }
+
+    }
+}
